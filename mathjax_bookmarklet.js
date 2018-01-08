129ae4b9e705bbc06416779e86532ce02ba263a4
anemochore <fallensky@naver.com>
2018-01-08 12:12:59 +0900

    Update mathjax_bookmarklet.js

--- mathjax_bookmarklet.js
+++ mathjax_bookmarklet.js
@@ -1,17 +1,19 @@
-var p = document.body;
-var g = document.createElement('script');
-var s = document.getElementsByTagName('script')[0];
-g.type = 'text/x-mathjax-config';
-g.text = 'MathJax.Hub.Config({tex2jax: {inlineMath: [["$","$"]],processEscapes: true}})';
+(() => {
+  let p = document.body;
+  let g = document.createElement('script');
+  let s = document.getElementsByTagName('script')[0];
+  g.type = 'text/x-mathjax-config';
+  g.text = 'MathJax.Hub.Config({tex2jax: {inlineMath: [["$","$"]],processEscapes: true}})';
 
-if(s === undefined) {
+  if(s === undefined) {
+    p.appendChild(g);
+    s = document.getElementsByTagName('script')[0];
+  }
+  else {
+    p.insertBefore(g, s);
+  }
+
+  g = document.createElement('script');
+  g.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML';
   p.appendChild(g);
-  s = document.getElementsByTagName('script')[0];
-}
-else {
-  p.insertBefore(g, s);
-}
-
-g = document.createElement('script');
-g.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML';
-p.appendChild(g);
+})();
