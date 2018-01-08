36c4fce32867b4b4ab68873f2f58a0ed03e6b49a
anemochore <fallensky@naver.com>
2018-01-08 12:09:30 +0900

    Add files via upload

---
+++ mathjax_bookmarklet.js
@@ -0,0 +1,17 @@
+var p = document.body;
+var g = document.createElement('script');
+var s = document.getElementsByTagName('script')[0];
+g.type = 'text/x-mathjax-config';
+g.text = 'MathJax.Hub.Config({tex2jax: {inlineMath: [["$","$"]],processEscapes: true}})';
+
+if(s === undefined) {
+  p.appendChild(g);
+  s = document.getElementsByTagName('script')[0];
+}
+else {
+  p.insertBefore(g, s);
+}
+
+g = document.createElement('script');
+g.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML';
+p.appendChild(g);
